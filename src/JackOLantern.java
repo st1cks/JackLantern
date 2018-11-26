6d1ba4fb9c3f43591c0ff2181643f43e8b8afcd8
BT_1N3_06@BKTECH.LOCAL <BT_1N3_06@BKTECH.LOCAL>
2018-11-26 14:45:51 -0500

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,11 @@
+public class JackOLantern {
+    private String[][] faceFeatures;
+
+    public JackOLantern(String[][] faceFeatures){
+        this.faceFeatures = faceFeatures;
+    }
+
+    public
+
+    }
+}
---
+++ src/JackOLanternRunner.java
@@ -0,0 +1,9 @@
+public class JackOLanternRunner {
+    public static void main(String[] args){
+        String[][] face = new String[5][8];
+        JackOLantern face1 = new Jack0Lantern(face);
+
+        face1.edit
+    }
+
+}
