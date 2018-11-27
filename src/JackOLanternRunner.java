cefe1b5ba49d4fc013d7fda3667509c8bd4430e4
BT_1N3_06 <rzhao3818@bths.edu>
2018-11-27 14:46:55 -0500

    help T-T

--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -1,9 +1,11 @@
 public class JackOLanternRunner {
     public static void main(String[] args){
         String[][] face = new String[5][8];
-        JackOLantern face1 = new Jack0Lantern(face);
+        JackOLantern face1 = new JackOLantern(face);
 
-        face1.edit
+        face1.fill("X");
+        face1.edit(" ",0,0,);
+        face1.edit(" "),
     }
 
 }
