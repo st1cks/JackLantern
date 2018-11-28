db8594dee56b8d16b65dfedf261d6062fb51cbe5
Ricky zhao <rzhao3818@bths.edu>
2018-11-27 23:50:55 -0500

    Finished classes, just need to finish lantern in Runner

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,11 +1,31 @@
 public class JackOLantern {
-    private String[][] faceFeatures;
+    private String[][] face;
 
-    public JackOLantern(String[][] faceFeatures){
-        this.faceFeatures = faceFeatures;
+    public JackOLantern(String[][] face){
+        this.face = face;
     }
 
-    public
+    public void fill(String line){
+    for (int a = 0; a < face.length; a++ ){
+        for (int b = 0; b < face[a].length; b++){
+            face[a][b] = line;
+        }
+    }
 
+    }
+
+    public void edit(String replace, int row, int column){
+        face[row][column] = replace;
+    }
+
+    public String toString(){
+        String Jack ="";
+        for (int a = 0; a < face.length; a++){
+            for(int b = 0; b < face[a].length; b++){
+                Jack = Jack+face[a][b];
+        }
+        Jack = Jack+"\n";
+    }
+    return Jack;
     }
 }
--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -1,11 +1,15 @@
 public class JackOLanternRunner {
     public static void main(String[] args){
-        String[][] face = new String[5][8];
+        String[][] face = new String[6][12];
         JackOLantern face1 = new JackOLantern(face);
 
         face1.fill("X");
-        face1.edit(" ",0,0,);
-        face1.edit(" "),
+        face1.edit(" ",0,0);
+
+
+        System.out.println(face1);
+
+
     }
 
 }
